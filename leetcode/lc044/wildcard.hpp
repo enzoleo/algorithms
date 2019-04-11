039fbcd81c9ca731bb46567409561904f3c51b59
enzoleo <liaopeiyu@outlook.com>
2019-04-11 14:55:47 +0800

    Update leetcode044 (better performance on efficiency)

--- leetcode/lc044/wildcard.hpp
+++ leetcode/lc044/wildcard.hpp
@@ -5,6 +5,11 @@
 #include <string>
 #include <cstring>
 
+/**
+ * Matrix definition.
+ * This is just a simple matrix class for solving the given problem.
+ * The kernel of this struct is the overloading of operator[].
+ */
 template<class T>
 struct Mat {
   constexpr Mat() = delete;
@@ -21,27 +26,40 @@ class Solution {
 public:
 
   static bool isMatch(const std::string& s, const std::string& t) {
+    // The size of the matrix. Note that the '+1' is important in this case.
     int rows = t.length() + 1, cols = s.length() + 1;
     bool *arr = new bool[rows * cols]();
 
+    // Matrix declaration and initialization.
     Mat<bool> mat(arr, rows, cols);
     mat[0][0] = true;
 
     for (int i = 1; i < rows; ++i) {
       bool flag = t[i - 1] == '*';
-      if (flag) mat[i][0] = mat[i - 1][0];
+      if (flag) {
+        // If the current character of string @t is '*', we only need to
+        // find the first substring of @s: s.substr(0, k+1) that can be
+        // perfectly matched by t.substr(0, i).
+        mat[i][0] = mat[i - 1][0];
+        int k = 0;
+        for (; k < cols; ++k)
+          if (mat[i - 1][k]) break;
+
+        // Now we find the objective @k, now with the character t[i-1] is
+        // '*', we can match all substring of @s: s.substr(0, len) with
+        // len >= k perfectly.
+        for (int iter = k; iter < cols; ++iter)
+          mat[i][iter] = true;
+        continue;
+      }
       for (int j = 1; j < cols; ++j) {
-        if (flag) {
-          for (int k = 0; k <= j; ++k)
-            if (mat[i - 1][k])
-              mat[i][j] = true;
-        }
-        else if (t[i - 1] == '?' || t[i - 1] == s[j - 1])
+        if (t[i - 1] == '?' || t[i - 1] == s[j - 1])
           mat[i][j] = mat[i - 1][j - 1];
         else mat[i][j] = false;
       }   
     }
     bool ans = mat[rows - 1][cols - 1];
+    // Remember to delete the space we dynamically allocated.
     delete[] arr;
     return ans;
   }
