dbdd489ecce2c4dc3c23ac7fafd595ad08a48971
enzoleo <enzoliao95@gmail.com>
2020-02-22 22:21:27 +0800

    Refactoring combat002

---
+++ combat/combat002/heap.h
@@ -0,0 +1,31 @@
+#ifndef HEAP_H_
+#define HEAP_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+// A simple struct of min-heap.
+typedef struct {
+  int size;
+  int capacity;
+  int* data;
+} Heap;
+
+// Researve space for heap.
+int reserve_heap(Heap* heap, int cap);
+
+// Always remember to free pointer space after using.
+void free_heap(Heap* heap);
+
+// Insert an element into the heap.
+int insert_heap(Heap* heap, int val);
+
+// Pop the top element (minimal one) of heap and reconstruct.
+int pop_heap(Heap* heap);
+
+// Print heap elements.
+void print_heap(Heap* heap);
+
+#endif
+
