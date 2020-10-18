5cef6bf97dbcb2c945dc5beacbc33aad46949858
enzoleo <enzoliao95@gmail.com>
2020-10-18 19:15:25 +0800

    Complete a basic solution of combat003 (still TLE? too slow)

--- combat/combat003/reflect.cpp
+++ combat/combat003/reflect.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <cstdio>
+#include <cstring>
 #include <vector>
+#include <limits>
 
 class BlackBox {
 public:  
   // Explicit constructor.
-  // The width and height must be specified to correctly instantiate a blackbox
+  // Width and height must be specified to correctly instantiate a blackbox
   // object. More specifically, the two arguments should be postive integers.
   explicit BlackBox(unsigned n, unsigned m)
       : w(m), h(n), status(2 * (m + n), false) { }
@@ -12,7 +15,7 @@ public:
   // Open the aperture, or activate the function of reflection.
   // Note that the validity of input argument @index will not been checked.
   // The return value is the final index of output aperture.
-  int open(int index, int direction) {
+  unsigned open(int index, int direction) {
     this->status[index] = true;
     do {
       // Find the next point on the border.
@@ -44,19 +47,109 @@ public:
     return 2 * w + 2 * h - index;    
   }
 
+  // Clear the status cache.
+  void clear() {
+    std::fill(status.begin(), status.end(), false);
+  }
+
+  // Resize the box with a new geometry pair of size.
+  // The status will also be resized to a specific size.
+  void resize(unsigned n, unsigned m) {
+    this->w = m;
+    this->h = n;
+    this->status.resize(2 * n * m, 0);
+  }
+
+  // Read specifications from a given file.
+  std::vector<unsigned> parse(const char* filename);
+
 private:
   // Blackbox geometry.
   unsigned w = 0, h = 0;
   std::vector<bool> status;
 };
 
-int main() {
-  BlackBox box(2, 3);
-  std::cout << box.open(6, -1) << std::endl;
-  std::cout << box.open(4, -1) << std::endl;
-  std::cout << box.open(0, -1) << std::endl;
-  box.close(6);
-  std::cout << box.open(0, -1) << std::endl;
+// Parse the external file.
+std::vector<unsigned> BlackBox::parse(const char* filename) {  
+  // Use C functions to open the text file.
+  FILE* fp = std::fopen(filename, "r");
+  if(!fp) { // Exit the program if file reading failed/
+    std::cerr << "File opening failed.\n";
+    exit(EXIT_FAILURE);
+  }
+  // Parse the actions about to be executed.
+  char status[5], delimiter;
+  std::vector<bool> action;
+  std::fscanf(fp, "[\"BlackBox\" %c", &delimiter);
+  while (delimiter == ',') {
+    std::fscanf(fp, " \"%[^\"]s", status);
+    std::fscanf(fp, "\" %c", &delimiter);
+    // Compare the action string with the two allowed options.
+    if (std::strcmp(status, "open") == 0) action.emplace_back(true);
+    else if (std::strcmp(status, "close") == 0) action.emplace_back(false);
+    else { // The action name is invalid.
+      std::cerr << "Recognizing action failed.\n";
+      exit(EXIT_FAILURE);
+    }
+  }
+  // Parse the index of apertures and corresponding direction of the light.
+  unsigned index; int direction, i = 0;
+  std::vector<unsigned> res;
+  std::fscanf(fp, "\n[[ %u , %u ] %c", &h, &w, &delimiter);
+  this->status.resize(2 * h * w);
+  while (delimiter == ',') {
+    if (action[i++]) {
+      // In this case, we are going to open an aperture.
+      int ret = std::fscanf(fp, " [ %u , %d ] %c",
+                            &index, &direction, &delimiter);
+      if (ret != 3) { // Not enough arguments are parsed.
+        std::cerr << "Arguments do not match.\n";
+        exit(EXIT_FAILURE);
+      }
+      // Compare the direction string with the two allowed options.
+      // Note that we do not check the validity of @index, so make sure that
+      // arguments of the input are not out of range.
+      if (direction == 1 || direction == -1)
+        res.emplace_back(this->open(index, direction));
+      else { // The action name is invalid.
+        std::cerr << "Recognizing direction failed.\n";
+        exit(EXIT_FAILURE);
+      }
+    } else {
+      // In this case, we are goint to close an aperture.
+      int ret = std::fscanf(fp, " [ %u ] %c", &index, &delimiter);
+      if (ret != 2) { // Not enough arguments are parsed.
+        std::cerr << "Arguments do not match.\n";
+        exit(EXIT_FAILURE);
+      }
+      this->close(index);
+      res.emplace_back(std::numeric_limits<unsigned>::max());
+    }
+  }
+  std::fclose(fp);
+  return std::move(res);
+}
+
+int main(int argc, char** argv) {
+  // Usage clarification.
+  if (argc != 2) {
+    const char usage[] = "Usage: \n"
+        "    ./reflect testcase.dat\n"
+        "Note: the text filename is allowed to be modified.\n"
+        "Sample input text file `testcase.dat`: \n"
+        "    [\"BlackBox\",\"open\",\"open\",\"open\",\"close\",\"open\"]\n"
+        "    [[2,3],[6,-1],[4,-1],[0,-1],[6],[0,-1]]";
+    std::cout << usage << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  BlackBox box(1, 1);
+  auto res = box.parse(argv[1]);
+
+  // Output the indices we obtain.
+  // Note that some action returns no value, so the corresponding return
+  // values are replaced with std::numerical_limits<unsigned>::max().
+  for (auto index : res)
+    std::cout << index << std::endl;
   return 0;
 }
 
